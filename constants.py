b2af77862272e10ba6b2ac6a57ac6b1bbfe8c8ed
Jacob Bom <bomjacob@gmail.com>
2016-05-21 14:54:29 +0200

    Fix soundcloud.

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ VOCADB_API_ENDPOINT = "http://vocadb.net/api/"
 OWNER_ID = 95205500
 DB_FILE = 'data.sqlite'
 voca_db_user_agent = 'Telegram-VocaDBBot/{}'.format(__version__)
-pvServices = ['Soundcloud', 'Youtube', 'NicoNicoDouga', 'Piapro', 'Vimeo', 'Bilibili']
+pvServices = ['SoundCloud', 'Youtube', 'NicoNicoDouga', 'Piapro', 'Vimeo', 'Bilibili']
